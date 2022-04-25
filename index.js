8cb2b6837c7fb69c44ea6aae702a187fb8b1cc45
unknown <nhquoc99@gmail.com>
2022-04-26 01:46:35 +0700

    completed navigation

---
+++ index.js
@@ -0,0 +1,15 @@
+//  when someone click the hamburer , if the menu is open -> close it and reverse
+const nav = document.querySelector(".primary-navigation")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+
+navToggle.addEventListener('click', () => {
+    const visibility = nav.getAttribute('data-visible')
+    
+    if(visibility === 'false') {
+        nav.setAttribute("data-visible", true)
+        navToggle.setAttribute('aria-expanded', true)
+    } else {
+        nav.setAttribute("data-visible", false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
+})
